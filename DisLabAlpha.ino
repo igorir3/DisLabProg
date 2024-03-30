07b0b93dcbff37c4607d3acd8eb6709ae3247a47
igorir3 <97892348+igorir3@users.noreply.github.com>
2024-03-30 17:04:04 +0300

    Add files via upload

--- DisLabAlpha.ino
+++ DisLabAlpha.ino
@@ -1,3 +1,5 @@
+#include <Wire.h>
+
 #define beta 4090
 #define resistance 10
 
@@ -38,6 +40,105 @@ int paramTermAnalogInput2 = 0;
 String i2c_rcv;
 int sender = 0;
 
+
+void dataRqst()
+{
+    switch (sender){
+    case 1:
+      long a = analogRead(TermistorOutput1);
+      float tempC = beta / (log((1025.0 * 10 / a - 10) / 10) + beta / 298.0) - 273.0;   // Цельсия
+      Wire.write((byte)tempC);
+    break;
+    case 2:
+      long b = analogRead(TermistorOutput2);
+      float tempC1 = beta / (log((1025.0 * 10 / b - 10) / 10) + beta / 298.0) - 273.0;  // Цельсия
+      Wire.write((byte)tempC1);
+    break;
+    case 3:
+      long c = analogRead(TermistorOutput3);
+      float tempC2 = beta / (log((1025.0 * 10 / c - 10) / 10) + beta / 298.0) - 273.0;
+      Wire.write((byte)tempC2);
+    break;
+    case 4:
+      long d = analogRead(TermistorOutput4);
+      float tempC3 = beta / (log((1025.0 * 10 / d - 10) / 10) + beta / 298.0) - 273.0;
+      Wire.write((byte)tempC3);
+    break;
+    case 5:
+      long e = analogRead(TermistorOutput5);
+      float tempC4 = beta / (log((1025.0 * 10 / e - 10) / 10) + beta / 298.0) - 273.0;
+      Wire.write((byte)tempC4);
+    break;
+    case 6:
+      long f = analogRead(TermistorOutput6);
+      float tempC5 = beta / (log((1025.0 * 10 / f - 10) / 10) + beta / 298.0) - 273.0;
+      Wire.write((byte)tempC5);
+    break;
+    case 7:
+      long motor1 = analogRead(MotorOutput1);
+      Wire.write((byte)motor1);
+    break;
+    case 8:
+      long motor2 = analogRead(MotorOutput2);
+      Wire.write((byte)motor2);
+    break;
+    case 9:
+      Wire.write(paramTermInputForward1);
+    break;
+    case 10:
+      Wire.write(paramTermInputForward2);
+    break;
+    case 11:
+      Wire.write(paramTermInputBack1);
+    break;
+    case 12:
+      Wire.write(paramTermInputBack2);
+    break;
+    case 13:
+      Wire.write(paramMotorAnalogInput1);
+    break;
+    case 14:
+      Wire.write(paramMotorAnalogInput2);
+    break;
+    case 15:
+      Wire.write(paramTermAnalogInput1);
+    break;
+    case 16:
+      Wire.write(paramTermAnalogInput2);
+    break;
+  }
+}
+
+void dataRcv(int numBytes) {  
+    if (numBytes > 2){
+      String i2c_rcv = "";
+      while(Wire.available()){
+        i2c_rcv += Wire.read();
+      }
+      Serial.print(" GET ");
+      Serial.println(i2c_rcv);
+      paramMotorAnalogInput1 = i2c_rcv.substring(5, 9).toInt() - 1000;
+      paramMotorAnalogInput2 = i2c_rcv.substring(9, 13).toInt() - 1000;
+      paramTermAnalogInput1 = i2c_rcv.substring(13, 17).toInt() - 1000;
+      paramTermAnalogInput2 = i2c_rcv.substring(17, 21).toInt() - 1000;
+      paramTermInputForward1 = i2c_rcv.substring(1, 2).toInt();
+      paramTermInputForward2 = i2c_rcv.substring(2, 3).toInt();
+      paramTermInputBack1 = i2c_rcv.substring(3, 4).toInt();
+      paramTermInputBack2 = i2c_rcv.substring(4, 5).toInt();
+    }
+    else{
+      byte i2c_rcv[2];
+      int x = 0;
+      while(Wire.available()){
+        i2c_rcv[x] = Wire.read();
+        x += 1;
+      }
+      int sender = i2c_rcv[0] | i2c_rcv[1] << 8;
+      Serial.print(" GET ");
+      Serial.println(sender);
+    }
+}
+
 void setup() {
   Wire.begin(44);
   Wire.onReceive(dataRcv);
@@ -74,91 +175,4 @@ void loop() {
   analogWrite(MotorAnalogInput2, paramMotorAnalogInput2);
   analogWrite(TermAnalogInput1, paramTermAnalogInput1);
   analogWrite(TermAnalogInput2, paramTermAnalogInput2);
-}
-
-void dataRqst()
-{
-    switch (sender){
-    case 1:
-      long a = analogRead(TermistorOutput1);
-      float tempC = beta / (log((1025.0 * 10 / a - 10) / 10) + beta / 298.0) - 273.0;   // Цельсия
-      Wire.write(tempC)
-    break;
-    case 2:
-      long b = analogRead(TermistorOutput2);
-      float tempC1 = beta / (log((1025.0 * 10 / b - 10) / 10) + beta / 298.0) - 273.0;  // Цельсия
-      Wire.write(tempC1)
-    break;
-    case 3:
-      long c = analogRead(TermistorOutput3);
-      float tempC2 = beta / (log((1025.0 * 10 / c - 10) / 10) + beta / 298.0) - 273.0;
-      Wire.write(tempC2)
-    break;
-    case 4:
-      long d = analogRead(TermistorOutput4);
-      float tempC3 = beta / (log((1025.0 * 10 / d - 10) / 10) + beta / 298.0) - 273.0;
-      Wire.write(tempC3)
-    break;
-    case 5:
-      long e = analogRead(TermistorOutput5);
-      float tempC4 = beta / (log((1025.0 * 10 / e - 10) / 10) + beta / 298.0) - 273.0;
-      Wire.write(tempC4)
-    break;
-    case 6:
-      long f = analogRead(TermistorOutput6);
-      float tempC5 = beta / (log((1025.0 * 10 / f - 10) / 10) + beta / 298.0) - 273.0;
-      Wire.write(tempC5)
-    break;
-    case 7:
-      long motor1 = analogRead(MotorOutput1);
-      Wire.write(motor1)
-    break;
-    case 8:
-      long motor2 = analogRead(MotorOutput2);
-      Wire.write(motor2)
-    break;
-    case 9:
-      Wire.write(paramTermInputForward1)
-    break;
-    case 10:
-      Wire.write(paramTermInputForward2)
-    break;
-    case 11:
-      Wire.write(paramTermInputBack1)
-    break;
-    case 12:
-      Wire.write(paramTermInputBack2)
-    break;
-    case 13:
-      Wire.write(paramMotorAnalogInput1)
-    break;
-    case 14:
-      Wire.write(paramMotorAnalogInput2)
-    break;
-    case 15:
-      Wire.write(paramTermAnalogInput1)
-    break;
-    case 16:
-      Wire.write(paramTermAnalogInput2)
-    break;
-}
-
-void dataRcv(int numBytes) {
-  while(Wire.available()) {
-    if (numBytes > 2){
-		  String i2c_rcv = Wire.read();
-      paramMotorAnalogInput1 = serialinput.substring(5, 9).toInt() - 1000;
-      paramMotorAnalogInput2 = serialinput.substring(9, 13).toInt() - 1000;
-      paramTermAnalogInput1 = serialinput.substring(13, 17).toInt() - 1000;
-      paramTermAnalogInput2 = serialinput.substring(17, 21).toInt() - 1000;
-      paramTermInputForward1 = serialinput.substring(1, 2).toInt();
-      paramTermInputForward2 = serialinput.substring(2, 3).toInt();
-      paramTermInputBack1 = serialinput.substring(3, 4).toInt();
-      paramTermInputBack2 = serialinput.substring(4, 5).toInt();
-    }
-    else{
-      int sender = Wire.read();
-    }
-    
-  }
 }
